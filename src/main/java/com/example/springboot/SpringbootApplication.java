d2bf4ba37d0ddd37ac043c4e4fdf03a869848a99
jayavardhanpatil <jayawardhan.patil@gmail.com>
2020-10-19 12:46:21 -0700

    Spring context and getBean

--- src/main/java/com/example/springboot/SpringbootApplication.java
+++ src/main/java/com/example/springboot/SpringbootApplication.java
@@ -1,13 +1,24 @@
 package com.example.springboot;
 
+import com.example.springboot.controllers.MyController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import java.applet.AppletContext;
 
 @SpringBootApplication
 public class SpringbootApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringbootApplication.class, args);
+
+		ApplicationContext context = SpringApplication.run(SpringbootApplication.class, args);
+
+		MyController controller = (MyController) context.getBean("myController");
+
+		String greeting = controller.helloWorld();
+		System.out.println(greeting);
+
 	}
 
 }
--- src/main/java/com/example/springboot/controllers/MyController.java
+++ src/main/java/com/example/springboot/controllers/MyController.java
@@ -1,4 +1,19 @@
 package com.example.springboot.controllers;
 
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: jayavardhanpatil
+ * Date: 10/19/20
+ * Time:  12:39
+ */
+@Controller
 public class MyController {
+
+    public String helloWorld(){
+        System.out.println("Hello World!");
+        return "Hello Man!";
+    }
+
 }
